7572d28a9f97c3129317f75b9e5de05e9007604f
SunQi <sun7127@126.com>
2022-04-07 16:32:43 +0800

    devlp:首次提交

---
+++ finduck-model/src/main/java/cn/finduck/model/FinduckModelApplication.java
@@ -0,0 +1,13 @@
+package cn.finduck.model;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinduckModelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinduckModelApplication.class, args);
+    }
+
+}
---
+++ finduck-model/src/test/java/cn/finduck/model/FinduckModelApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.finduck.model;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinduckModelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
